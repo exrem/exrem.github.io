66cdfa3a55a6ea0656fef57dd3190d835f7cda09
Eray <eray6421@proton.me>
2024-01-06 23:18:32 +0400

    an empty project :3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,19 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Əkrəm Cəfər</title>
+	<meta
+		name="description"
+		content="My minimalistic single-page website to be used in social media bio links."
+	/>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<main></main>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+	@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap");
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	:global(body) {
+		background: black;
+		color: white;
+		font-family: "Manrope", sans-serif;
 	}
 </style>
